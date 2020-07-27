56b3c204279e7a50b413ce6c3d391fef466a8063
nguyenvantung9296 <tungnguyenvan9296@gmail.com>
2020-07-28 00:11:01 +0700

    update-code-login

--- js/login.js
+++ js/login.js
@@ -1,61 +1,115 @@
+/*global $, document, window, setTimeout, navigator, console, location*/
+$(document).ready(function () {
 
-$('#form-resi').on('submit', function() {
-	/* Act on the event */
-    var istrue = true;
-   if($('#login-input').val().trim() ==''){
-		$('#login-input').next('span').text('***');
-		isTrue = false;
-	}else{
-		$('#login-input').next('span').text('');
+    'use strict';
+
+    var usernameError = true,
+        emailError    = true,
+        passwordError = true,
+        passConfirm   = true;
+
+    // Detect browser for css purpose
+    if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
+        $('.form form label').addClass('fontSwitch');
     }
+
+    // Label effect
+    $('input').focus(function () {
+
+        $(this).siblings('label').addClass('active');
+    });
+
+    // Form validation
+    $('input').blur(function () {
+
+        // User Name
+        if ($(this).hasClass('name')) {
+            if ($(this).val().length === 0) {
+                $(this).siblings('span.error').text('Bạn cần nhập thông tin họ tên').fadeIn().parent('.form-group').addClass('hasError');
+                usernameError = true;
+            } else if ($(this).val().length > 1 && $(this).val().length <= 6) {
+                $(this).siblings('span.error').text('Please type at least 6 characters').fadeIn().parent('.form-group').addClass('hasError');
+                usernameError = true;
+            } else {
+                $(this).siblings('.error').text('').fadeOut().parent('.form-group').removeClass('hasError');
+                usernameError = false;
+            }
+        }
+        // Email
+        if ($(this).hasClass('email')) {
+            if ($(this).val().length == '') {
+                $(this).siblings('span.error').text('Bạn cần nhập thông tin email').fadeIn().parent('.form-group').addClass('hasError');
+                emailError = true;
+            } else {
+                $(this).siblings('.error').text('').fadeOut().parent('.form-group').removeClass('hasError');
+                emailError = false;
+            }
+        }
+
+        // PassWord
+        if ($(this).hasClass('pass')) {
+            if ($(this).val().length < 8) {
+                $(this).siblings('span.error').text('Mật khẩu tối thiểu có 8 kí tự').fadeIn().parent('.form-group').addClass('hasError');
+                passwordError = true;
+            } else {
+                $(this).siblings('.error').text('').fadeOut().parent('.form-group').removeClass('hasError');
+                passwordError = false;
+            }
+        }
+
+        // PassWord confirmation
+        if ($('.pass').val() !== $('.passConfirm').val()) {
+            $('.passConfirm').siblings('.error').text('Mật khẩu không hợp lệ').fadeIn().parent('.form-group').addClass('hasError');
+            passConfirm = false;
+        } else {
+            $('.passConfirm').siblings('.error').text('').fadeOut().parent('.form-group').removeClass('hasError');
+            passConfirm = false;
+        }
+
+        // label effect
+        if ($(this).val().length > 0) {
+            $(this).siblings('label').addClass('active');
+        } else {
+            $(this).siblings('label').removeClass('active');
+        }
+    });
+
+
+    // form switch
+    $('a.switch').click(function (e) {
+        $(this).toggleClass('active');
+        e.preventDefault();
+
+        if ($('a.switch').hasClass('active')) {
+            $(this).parents('.form-peice').addClass('switched').siblings('.form-peice').removeClass('switched');
+        } else {
+            $(this).parents('.form-peice').removeClass('switched').siblings('.form-peice').addClass('switched');
+        }
+    });
+
+
+    // Form submit
+    $('form.signup-form').submit(function (event) {
+        event.preventDefault();
+
+        if (usernameError == true || emailError == true || passwordError == true || passConfirm == true) {
+            $('.name, .email, .pass, .passConfirm').blur();
+        } else {
+            $('.signup, .login').addClass('switched');
+
+            setTimeout(function () { $('.signup, .login').hide(); }, 700);
+            setTimeout(function () { $('.brand').addClass('active'); }, 300);
+            setTimeout(function () { $('.heading').addClass('active'); }, 600);
+            setTimeout(function () { $('.success-msg p').addClass('active'); }, 900);
+            setTimeout(function () { $('.success-msg a').addClass('active'); }, 1050);
+            setTimeout(function () { $('.form').hide(); }, 700);
+        }
+    });
+
+    // Reload page
+    $('a.profile').on('click', function () {
+        location.reload(true);
+    });
+
+
 });
-$('#form-resi').on('submit', function() {
-	/* Act on the event */
-    var istrue = true;
-   if($('#password-input').val().trim() ==''){
-		$('#password-input').next('span').text('***');
-		isTrue = false;
-	}else{
-		$('#password-input').next('span').text('');
-    }
-});
-
-
-
-function submitButton(){
-	$('#form-login').validate({
-		rules : {
-            login :{
-           	required: true,
-           },
-           password :{
-           	required: true,
-           }
-          }
-	});
-	let validatedata= $('#form-login').valid();
-	if(validatedata == true){
-		$('#form-login').submit();
-	}
-};
-
-// 	$(document).ready(function() {
-// 		$(".click-login").on("click", function() {
-// 			$('.login-shose1').addClass('opacity');
-// 			$('.registration').addClass('show');
-// 		});	   
-//   });
-
-
-// 	$(document).ready(function() {
-// 		$('.click-login').click(function(){
-// 			$('#login-shose1').hide();
-// 			$('.registration').show();
-// 			});
-//   });
-
-
-
-
-
-		
